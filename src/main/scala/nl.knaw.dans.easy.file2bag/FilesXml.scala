0edbf851faf7f8b5e3aad01c73bd48ee1818aec0
jo-pol <jo-pol@users.noreply.github.com>
2020-06-18 15:47:45 +0200

    finished FilesXml

--- src/main/scala/nl.knaw.dans.easy.file2bag/FilesXml.scala
+++ src/main/scala/nl.knaw.dans.easy.file2bag/FilesXml.scala
@@ -19,26 +19,11 @@ import java.nio.file.Path
 
 import better.files.File
 
-import scala.util.Try
+import scala.util.{ Success, Try }
 import scala.xml.transform.{ RewriteRule, RuleTransformer }
-import scala.xml.{ Elem, Node }
+import scala.xml.{ Elem, Node, XML }
 
 object FilesXml {
-
-  private def addItem(oldFiles: Node, newItem: Node): Try[Node] = Try {
-    // copied from https://github.com/DANS-KNAW/easy-ingest-flow/blob/de2c163335808e71992ee620391a056c344562c9/src/test/scala/nl.knaw.dans.easy.ingestflow/flowsteps/FlowStepEnrichMetadataSpec.scala#L80-L92
-    object insertElement extends RewriteRule {
-      override def transform(node: Node): Seq[Node] = node match {
-        case Elem(boundPrefix, "files", _, boundScope, children @ _*) =>
-          <files>
-            { children }
-            { newItem }
-          </files>.copy(prefix = boundPrefix, scope = boundScope)
-        case other => other
-      }
-    }
-    new RuleTransformer(insertElement).transform(oldFiles).head
-  }
 
   /**
    * @param oldFilesXml the old metadata/dataset.xml of the bag
@@ -47,15 +32,47 @@ object FilesXml {
    * @return
    */
   def apply(oldFilesXml: Elem, rights: String, path: Path, datasetXml: File): Try[Node] = {
-    val visibleTo = "ANONYMOUS" // TODO if right is empty then from dataset
-    val accessibleTo = "ANONYMOUS" // TODO what?
-    val newItem =
+
+    def newItem(accessibleTo: String) = {
       <file filepath={ "data/" + path }>
         <dcterms:title>{ path.getFileName }</dcterms:title>
         <!-- <dcterms:format>{ ??? }</dcterms:format> -->
         <accessibleToRights>{ accessibleTo }</accessibleToRights>
-        <visibleToRights>{ visibleTo }</visibleToRights>
+        <visibleToRights>{ visibleTo(accessibleTo) }</visibleToRights>
       </file>
-    addItem(oldFilesXml, newItem)
+    }
+
+    def addItem(oldFiles: Node, accessibleTo: String): Try[Node] = Try {
+      // copied from https://github.com/DANS-KNAW/easy-ingest-flow/blob/de2c163335808e71992ee620391a056c344562c9/src/test/scala/nl.knaw.dans.easy.ingestflow/flowsteps/FlowStepEnrichMetadataSpec.scala#L80-L92
+      object insertElement extends RewriteRule {
+        override def transform(node: Node): Seq[Node] = node match {
+          case Elem(boundPrefix, "files", _, boundScope, children @ _*) =>
+            <files>
+            { children }
+            { newItem(accessibleTo) }
+          </files>.copy(prefix = boundPrefix, scope = boundScope)
+          case other => other
+        }
+      }
+      new RuleTransformer(insertElement).transform(oldFiles).head
+    }
+
+    val tryAccessibleTo = {
+      if (!rights.isBlank) Success(rights)
+      else Try {
+        val ddm = XML.loadFile(datasetXml.toString) // may throw something
+        (ddm \ "profile" \ "accessRights").text
+      }
+    }
+
+    for {
+      accessibleTo <- tryAccessibleTo
+      newFilesXml <- addItem(oldFilesXml, accessibleTo)
+    } yield newFilesXml
+  }
+
+  private def visibleTo(accessibleTo: String) = accessibleTo match {
+    case "NONE" => "NONE"
+    case _ => "ANONYMOUS"
   }
 }
